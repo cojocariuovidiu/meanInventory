61cfb301b1055b76f88550fd2409637f8355217a
Mazen <mmmerhi@avaya.com>
2016-04-29 22:15:55 -0500

    Changing data model

--- server/controllers/ItemController.js
+++ server/controllers/ItemController.js
@@ -2,15 +2,15 @@
  * Created by Mazen.
  */
 
-
 var Resource   = require ( 'resourcejs' );
+
 
 module.exports = function( app, route ) {
 
-    // Setup the controller for REST;
+    // Setup the controller for REST
     Resource ( app, '', route, app.models.item ).rest ();
 
-    // Return middleware.
+    // Return middleware
     return function( req, res, next ) {
         next ();
     };
